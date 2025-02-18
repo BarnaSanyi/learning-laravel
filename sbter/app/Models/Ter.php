c69d6a0ecc732d49380fe30bb7772ea44e0234e7
Sandor Barnabas <h378211@stud.u-szeged.hu>
2025-02-18 23:34:19 +0100

    Create Ters done

--- sbter/app/Models/Ter.php
+++ sbter/app/Models/Ter.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ter extends Model
 {
-    //
+    protected $fillable = [
+        'message',
+    ];
 }
--- sbter/resources/views/layouts/navigation.blade.php
+++ sbter/resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('ters.index')" :active="request()->routeIs('ters.index')">
+                        {{ __('Ters') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -69,6 +72,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('ters.index')" :active="request()->routeIs('ters.index')">
+                {{ __('Ters') }}
             </x-responsive-nav-link>
         </div>
 
